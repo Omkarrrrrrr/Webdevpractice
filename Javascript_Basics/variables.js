2b295ecb4a3718ec5f3add7de2fe6f9f890b1d6f
Omkarrrrrrr <ominalawade77@gmail.com>
2024-09-07 10:46:17 +0530

    first commit

---
+++ Javascript_Basics/variables.js
@@ -0,0 +1,16 @@
+let x = 100;        //instruction to create a variable
+console.log(x);     //instructiom to print value present in variable
+
+const y = 99;
+console.log(y);
+
+var z = 56;
+console.log(z);
+
+let productName = "Iphone 13 pro max";
+const productPrice = 125000;
+var company = "Apple";
+
+console.log("Name of the product is :", productName);
+console.log("Price of the product is:" , productPrice);
+console.log("Name of the company is :", company);
