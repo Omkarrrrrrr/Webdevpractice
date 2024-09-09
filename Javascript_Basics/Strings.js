2d7baf7b1cd72787e1763edb59fadbdc538c25d8
Omkarrrrrrr <ominalawade77@gmail.com>
2024-09-09 09:44:12 +0530

    strings

---
+++ Javascript_Basics/Strings.js
@@ -0,0 +1,17 @@
+let singleQuoted = 'Omkar';
+let doubleQuoted = "Omkar";
+let backTicked = `Omkar`;
+
+
+console.log(singleQuoted,doubleQuoted,backTicked);
+
+
+// console.log("He said : "my name is omkar ")    here we can use single  quote
+
+// console.log('Good Morning , I'm Omkar')        here we can use double quotes
+
+// Backticked is used for multiline strings
+
+console.log(`this is
+    multiline 
+    string`)
