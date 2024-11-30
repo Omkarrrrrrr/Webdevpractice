223c1a9b96e30e3c09ed4c3dd5710d72d8514ca3
Omkarrrrrrr <ominalawade77@gmail.com>
2024-11-30 12:31:00 +0530

    operators

---
+++ Javascript_Basics/Operators and loops/AssigmentOp.js
@@ -0,0 +1,18 @@
+let a = 6;
+let b = 8;
+
+a == 7;
+console.log(a == 7); // false
+
+a=7;
+console.log()
+console.log(a==7); //now true
+
+console.log(a += b);   // a = a + b
+                       // ex . here a = 7 + 8=15;
+
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
+console.log(a **= b);
---
+++ Javascript_Basics/Operators and loops/ComparisonOp.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 6;
+
+console.log(a == b);
+console.log(a != b);
+
+console.log(a === b);      // false bcoz values are inequal
+console.log( a === "a");   // false becoz both type and values are inequal
+console.log(a === a);    // true bcoz  values and type both are equal
+
+console.log(a != b);
+console.log(a!==b);
+console.log(a < b);
+console.log(a > b);
+console.log(a >= b);
+console.log(a <= b);
