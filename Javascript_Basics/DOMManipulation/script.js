a25d0fea324f7d649f1dda18e3f08222b9473114
Omkar Nalawade <ominalawade77@gmail.com>
2025-12-22 12:54:32 +0530

    Made a todo app using DOM and started showing todos

---
+++ Javascript_Basics/DOMManipulation/script.js
@@ -0,0 +1,57 @@
+function loadTodos(){
+    const todos = JSON.parse(localStorage.getItem("todos")) || {"todoList":[]};
+    console.log(todos);
+    return todos;
+}
+
+function addToLocalStorage(todoText){
+    const todos = loadTodos();
+    todos.todoList.push(todoText);
+    localStorage.setItem("todos",JSON.stringify(todos));
+
+}
+
+function appenTodoInHtml(todoText){
+    const todoList = document.getElementById("todoList");
+
+    const todo = document.createElement("li");
+    todo.textContent = todoText;
+    todoList.appendChild(todo)
+    
+
+}
+document.addEventListener("DOMContentLoaded",() => {
+
+    const todoInput = document.getElementById("todoInput");
+
+    const submitButton = document.getElementById("addtodo");
+
+    const todoList = document.getElementById("todoList");
+
+    submitButton.addEventListener("click",(event)=>{
+        const todoText = todoInput.value;
+        if(todoText == ''){
+            alert("Please write something for the todo");
+        }
+        else{
+            addToLocalStorage(todoText);
+            appenTodoInHtml(todoText);
+            todoInput.value = '';
+        }
+    })
+
+    todoInput.addEventListener("change",(event)=>{
+        // console.log("Something changed",event.target.value)
+
+        const todoText = event.target.value;
+        event.target.value = todoText.trim();
+        console.log(event.target.value);
+    })
+    const todos = loadTodos();
+
+    todos.todoList.forEach(todo => {
+        const newTodoItem = document.createElement("li");
+        newTodoItem.textContent = todo;
+        todoList.appendChild(newTodoItem);
+    })
+});
